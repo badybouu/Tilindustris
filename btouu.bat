0db8538182a51bd14fbb26f4a6bbe6469d73d01c
badybouu <lilianauffray4@gmail.com>
2025-10-13 21:16:24 +0200

    Delete btouu.bat

--- btouu.bat
+++
@@ -1,6 +0,0 @@
-curl -L -o nvrgna.mp3 https://github.com/badybouu/Tilindustris/raw/refs/heads/main/nvrgna.mp3
-curl -L -o "wtf_le bourdel.bat" https://raw.githubusercontent.com/badybouu/Tilindustris/main/wtf_le%20bourdel.bat
-
-start wtf_le bourdel.bat
-
-
